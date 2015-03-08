78ecb363f0913cdb7523ec2e9ccfd94883e6d045
Ken Ishimoto <ken@ksroom.com>
2015-03-08 19:59:42 +0100

    remove old javadoc

--- wolips/core/plugins/org.objectstyle.wolips.templateengine/ProjectTemplates/TB Application Extended/Sources/__basePackage_folder__/Session.java
+++ wolips/core/plugins/org.objectstyle.wolips.templateengine/ProjectTemplates/TB Application Extended/Sources/__basePackage_folder__/Session.java
@@ -14,11 +14,7 @@ import org.treasureboat.foundation.constants.TBFKnownLookNames;
 import org.treasureboat.webcore.appserver.navbar.handler.TBNavigationHandler;
 
 public class Session extends TBSession {
-	/**
-	 * Do I need to update serialVersionUID?
-	 * See section 5.6 <cite>Type Changes Affecting Serialization</cite> on page 51 of the 
-	 * <a href="http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf">Java Object Serialization Spec</a>
-	 */
+
 	private static final long serialVersionUID = 1L;
    
 	/** 
