a83c7fb8608f7e52e1e55b78c715cabf73bff67a
Ken Ishimoto <ken@ksroom.com>
2015-03-11 15:47:45 +0100

    create wodka user for BaseModel

--- wolips/core/plugins/org.objectstyle.wolips.templateengine/ProjectTemplates/TB Application Extended/Sources/__basePackage_folder__/override/FirstDataCreator.java
+++ wolips/core/plugins/org.objectstyle.wolips.templateengine/ProjectTemplates/TB Application Extended/Sources/__basePackage_folder__/override/FirstDataCreator.java
@@ -2,14 +2,14 @@ package ${basePackage}.override;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-#if ($pro)
+#if ($pro || $base)
 	import org.treasureboat.basemodel.override.TBBM_FirstDataCreator;
 #else
 	import org.treasureboat.webcore.override.TBWInitializerOfFirstDataCreator;
 #end
 import org.treasureboat.webcore.security.grant.TBWGrantAccess;
 
-#if ($pro)
+#if ($pro || $base)
 public class FirstDataCreator extends TBBM_FirstDataCreator {
 #else
 public class FirstDataCreator extends TBWInitializerOfFirstDataCreator { 
@@ -18,7 +18,6 @@ public class FirstDataCreator extends TBWInitializerOfFirstDataCreator {
 	/** 
 	 * <a href="http://wiki.wocommunity.org/display/documentation/Wonder+Logging">new org.slf4j.Logger</a> 
 	 */
-	@SuppressWarnings("hiding")
 	static final Logger log = LoggerFactory.getLogger(FirstDataCreator.class);
 
 	//********************************************************************
