411d583f82f0695abb75bb12051342cf7d414ded
Ken Ishimoto <ken@ksroom.com>
2015-03-11 15:47:59 +0100

    fix LoginCheck for BaseModel

--- wolips/core/plugins/org.objectstyle.wolips.templateengine/ProjectTemplates/TB Application Extended/Sources/__basePackage_folder__/override/LoginCheck.java
+++ wolips/core/plugins/org.objectstyle.wolips.templateengine/ProjectTemplates/TB Application Extended/Sources/__basePackage_folder__/override/LoginCheck.java
@@ -1,12 +1,12 @@
 package ${basePackage}.override;
 
-#if ($pro)
+#if ($pro || $base)
 import org.treasureboat.basemodel.override.TBBM_LoginCheck;
 #else
 import org.treasureboat.webcore.security.password.TBWLoginBaseCheck;
 #end
 
-#if ($pro)
+#if ($pro || $base)
 	public class LoginCheck extends TBBM_LoginCheck {
 #else
 	public class LoginCheck extends TBWLoginBaseCheck {
